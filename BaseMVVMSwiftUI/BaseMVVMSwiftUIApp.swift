6d5f6e6e41ea85a506f2eb9dc40d9f7a0c56f31c
Junior Margalho <juniormargalho@brq.com>
2023-05-15 14:01:56 -0300

    step: initial

---
+++ BaseMVVMSwiftUI/BaseMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseMVVMSwiftUIApp.swift
+//  BaseMVVMSwiftUI
+//
+//  Created by Junior Margalho on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BaseMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
